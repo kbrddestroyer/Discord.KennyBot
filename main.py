fc44804d8e03353bd1f2e1a97875b41885213ef9
kbrddestroyer <grudko2003@gmail.com>
2024-06-26 18:54:46 +0300

    Initial

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import interactions
+
+import os
+
+# Создаем бота
+Bot = commands.Bot(
+    intents=discord.Intents.default(),
+    command_prefix="!"
+)
+
+@Bot.event
+async def on_message(ctx):
+    print(ctx)
+    if ctx.author != Bot.user:
+        await ctx.reply("Bruh")
+    
+config = {
+    'token': os.environ['TOKEN']
+}
+
+Bot.run(config['token'])
+
